9b1c7eac119d0af508593a6a5871043157ab26b3
foo-543674 <foo.avoutar7654@gmail.com>
2025-05-25 17:21:55 +0000

    [update]use implemented modules

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
+use std::sync::Arc;
+
 use fs_text_search_mcp::servers::search::SearchServer;
 use rmcp::{ServiceExt, transport::stdio};
 use tracing_subscriber::{self, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::DEBUG.into()))
-        .with_writer(std::io::stderr)
-        .with_ansi(false)
-        .init();
+  tracing_subscriber::fmt()
+    .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::DEBUG.into()))
+    .with_writer(std::io::stderr)
+    .with_ansi(false)
+    .init();
 
-    tracing::info!("Starting Server...");
+  tracing::info!("Starting Server...");
 
-    let service = SearchServer::new().serve(stdio()).await.inspect_err(|e| {
-        tracing::error!("serving error: {:?}", e);
+  let file_loader = Arc::new(fs_text_search_mcp::file::lazy_file_loader::LazyFileLoader::new());
+  let service = SearchServer::new("foo".to_string(), file_loader)
+    .serve(stdio())
+    .await
+    .inspect_err(|e| {
+      tracing::error!("serving error: {:?}", e);
     })?;
 
-    service.waiting().await?;
-    Ok(())
+  service.waiting().await?;
+  Ok(())
 }
